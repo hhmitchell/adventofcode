1425dc51dc96e28251ec6e4f8374914d39d1b289
Henry Mitchell <hhmitchell@gmail.com>
2021-12-12 21:19:46 -0800

    Day 07 part 1.

---
+++ 2021/day07part1.py
@@ -0,0 +1,30 @@
+#! /usr/bin/env python
+
+import sys
+
+
+def main(args):
+    crabs = [0] * 9
+    with open(args[0], 'r') as f:
+        for line in f.readlines():
+            for n in line.strip().split(','):
+                num = int(n)
+                if len(crabs) < num + 1:
+                    crabs.extend([0] * (num + 1 - len(crabs)))
+                crabs[num] += 1
+
+    #print(crabs)
+
+    fuel = [0] * len(crabs)
+    for i in range(len(crabs)):
+        for j in range(len(crabs)):
+            fuel[i] += crabs[j] * abs(i - j)
+
+    #print(fuel)
+    print(fuel.index(min(fuel)))
+    print(min(fuel))
+
+
+if __name__ == '__main__':
+    args = sys.argv[1:]
+    main(args)
